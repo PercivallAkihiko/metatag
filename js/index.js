56a9f5b9ef25c9f27363363b379b430a10ae227c
PercivallAkihiko <37913210+PercivallAkihiko@users.noreply.github.com>
2024-03-14 11:49:11 +0100

    Added contact section

--- js/index.js
+++ js/index.js
@@ -2,15 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
     initMenuButton();
 });
 
-function initMenuButton(){
+function initMenuButton(){     
     var explanation = document.querySelector(".explanation");   
-    var team = document.querySelector(".team");  
-
+    var team = document.querySelector(".team");      
+    var contact = document.querySelector(".contact");      
+    
     var explanationTarget = document.getElementById('explanation_target');    
-    var teamTarget = document.getElementById('team_target');    
-
+    var teamTarget = document.getElementById('team_target');          
+    var contactTarget = document.getElementById('contact_target'); 
+    
     addScrollListener(explanation, explanationTarget);
     addScrollListener(team, teamTarget);
+    addScrollListener(contact, contactTarget);
 }
 
 function addScrollListener(element, target, headerHeight=94) {
