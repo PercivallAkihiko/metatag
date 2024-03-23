3e815df0bad01222b35d75d0aa50f3af11ad6bac
IReallyLikeYourPants <michelekryston@gmail.com>
2024-03-23 01:05:09 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -304,14 +304,14 @@ const validTags = [
     "breakthrough"
 ];
 
-// 1 BuyTokens(                  address indexed user, 					uint amount,
-// 2 ReceiveTokensFromValidator( address indexed validator, 					uint amount,
-// 3 SetVariable(                address indexed validator, 					bool indexed value,
-// 4 SubmitHash(                 address indexed validator, 	address indexed company, 	bytes32 hash, 				uint indexed videoId, 
-// 5 RevealHash(                 address indexed validator, 	address indexed company, 	uint[] tags, bytes11 seed uint, 	uint indexed videoId, 
-// 6 GetRewards(                 address indexed validator, 	address indexed company, 	uint rewardAmount, bool positive, 	uint indexed videoId, 
-// 7 WithdrawTokensValidator(    address indexed validator, 					uint amount
-// 8 MTGforVoucher(              address indexed user,
+// 1 BuyTokens(                  address indexed user, 					        uint amount)
+// 2 ReceiveTokensFromValidator( address indexed validator, 					uint amount)
+// 3 SetVariable(                address indexed validator, 					bool indexed value
+// 4 SubmitHash(                 address indexed validator, 	address indexed company, 	bytes32 hash, 				        uint indexed videoId)
+// 5 RevealHash(                 address indexed validator, 	address indexed company, 	uint[] tags, bytes11 seed uint, 	uint indexed videoId)
+// 6 GetRewards(                 address indexed validator, 	address indexed company, 	uint rewardAmount, bool positive, 	uint indexed videoId)
+// 7 WithdrawTokensValidator(    address indexed validator, 					uint amount)
+// 8 MTGforVoucher(              address indexed user)
 
 var eventsDB = [
     /* // BuyTokens(                  address indexed user, 					uint amount
