8ee8cb6a68fccf2ab749a837f32966b7ab1152c2
PercivallAkihiko <37913210+PercivallAkihiko@users.noreply.github.com>
2024-03-25 11:15:35 +0100

    Minor change to the top menu
    Click on the logo to go up

--- js/index.js
+++ js/index.js
@@ -3,21 +3,33 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function initMenuButton(){     
+    var logo = document.querySelector('.logo_container'); 
+    var logoName = document.querySelector('.logo_name_container'); 
     var explanation = document.querySelector(".explanation");   
     var team = document.querySelector(".team");      
-    var contact = document.querySelector(".contact");      
-    
+    var contact = document.querySelector(".contact");          
+        
     var explanationTarget = document.getElementById('explanation_target');    
     var teamTarget = document.getElementById('team_target');          
     var contactTarget = document.getElementById('contact_target'); 
     
+    addScrollListener(logo);
+    addScrollListener(logoName);
     addScrollListener(explanation, explanationTarget);
     addScrollListener(team, teamTarget);
-    addScrollListener(contact, contactTarget);
+    addScrollListener(contact, contactTarget);    
 }
 
-function addScrollListener(element, target, headerHeight=94) {
+function addScrollListener(element, target = "TOP", headerHeight=94) {
     element.addEventListener('click', function() {
+        if(target == "TOP"){
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth' // Optional: smooth scrolling behavior
+            });
+            return;
+        }
+
         const offset = target.offsetTop - headerHeight;
         window.scrollTo({
             top: offset,
